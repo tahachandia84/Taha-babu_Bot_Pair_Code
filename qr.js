b12b14709161b7495c9ae391d68666101358de4d
Taha khan <tahachandia84@gmail.com>
2026-06-09 20:10:17 +0500

    Update qr.js

--- qr.js
+++ qr.js
@@ -150,17 +150,17 @@ router.get('/', async (req, res) => {
                             
                             // Send video thumbnail with caption
                             await sock.sendMessage(userJid, {
-                                image: { url: 'https://img.youtube.com/vi/-oz_u1iMgf8/maxresdefault.jpg' },
-                                caption: `🎬 *KnightBot MD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/NjOipI2AoMk`
+                                image: { url: 'https://i.imgur.com/1tUVG85.jpeg' },
+                                caption: `🎬 *tahaBot MD V2.0 Full Setup Guide!*\n\n🚀 Bug Fixes + New Commands + Fast AI Chat\n📺 Watch Now: https://youtu.be/tahakhan`
                             });
                             console.log("🎬 Video guide sent successfully");
                             
                             // Send warning message
                             await sock.sendMessage(userJid, {
                                 text: `⚠️Do not share this file with anybody⚠️\n 
-┌┤✑  Thanks for using Knight Bot
+┌┤✑  Thanks for using taha khan Bot
 │└────────────┈ ⳹        
-│©2025 Mr Unique Hacker 
+│©2025 taha khan
 └─────────────────┈ ⳹\n\n`
                             });
                         } else {
